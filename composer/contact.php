c8bf38eb98242315ada481c3d03ee264831bed41
Eemil74 <Eemil.k.k.2007@gmail.com>
2024-08-19 15:32:41 +0300

    ...

---
+++ composer/contact.php
@@ -0,0 +1,37 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "eemil";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $name = mysqli_real_escape_string($conn, $name);
+    $email = mysqli_real_escape_string($conn, $email);
+    $subject = mysqli_real_escape_string($conn, $subject);
+    $message = mysqli_real_escape_string($conn, $message);
+
+    $sql = "INSERT INTO messages (name1, email1, subject1, message1) VALUES ('$name', '$email', '$subject', '$message')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+// Close
+$conn->close();
+header("location:../contact.html");
+?>
